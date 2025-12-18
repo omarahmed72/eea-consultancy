269cbe74670ef1353d5363e0c4874bcf2ab2e386
omarahmed72 <81181540+omarahmed72@users.noreply.github.com>
2025-12-18 11:49:31 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function () {
       rect.top >= 0 &&
       rect.left >= 0 &&
       rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
+      (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
